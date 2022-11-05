5f7fc9093246f19bdf35bcc7d5e8d06df9c4af76
ozcgokberk <ozcangokberk06@gmail.com>
2022-11-05 13:46:40 +0300

    -yeni cevap eklendi.
    5.2 sorusu eklendi.

--- GokberkOzcanHW_1.playground/Contents.swift
+++ GokberkOzcanHW_1.playground/Contents.swift
@@ -54,7 +54,7 @@ func makeAEqualPyramid(level: Int){
 makeAEqualPyramid(level: 5)
 print("---------------Question 5: ---------------")
 
-print("5.1:")
+print("----5.1:----")
 var isGoingToSum: [Int] = []
 var sum = 0
 for i in 0...1000 {
@@ -65,11 +65,28 @@ for i in 0...1000 {
     }
 }
 print(sum)
-print("5.2:")
+print("----5.2:----")
+    let myNumber: Int = 2
+    func myFun() -> Int {
+        
+        var start = 0
+        var num = 1
+        var sum = 0
+        
+        while num < 4_000_000 {
+            let x = start + num
+            start = num
+            num = x
+            
+            if num % 2 == 0 {
+                sum += num
+            }
+        }
+        return sum
+    }
+print(myFun())
 
-
-
-print("5.3:")
+print("----5.3----:")
 // tekrar bak.
 let number: Int = 3
 
